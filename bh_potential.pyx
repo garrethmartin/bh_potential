758487ce22d1c08e49cb44ceacb020e9bcaa4886
garrethmartin <garreth.martin1@gmail.com>
2024-11-19 21:34:42 +0000

    improved memory management

--- bh_potential.pyx
+++ bh_potential.pyx
@@ -5,12 +5,13 @@ from libc.math cimport sqrt
 import numpy as np
 cimport numpy as np
 
-
+# constants
 cdef double G = 4.30091e-6 # big G for units of kpc km^2 / Msun / s^2
 cdef double THETA = 0.5  # Barnes-Hut accuracy threshold
 cdef int MAX_OCTREE_DEPTH = 16
 
-cdef double offsets[8][3] # offsets for building octree
+# offsets used for subdividing octree
+cdef double offsets[8][3]
 offsets[:] = [
     [1,  1,  1],
     [1,  1, -1],
@@ -22,6 +23,167 @@ offsets[:] = [
     [-1, -1, -1],
 ]
 
+cdef class Octree:
+    """
+    Initialize the octree and insert particles into it.
+
+    Parameters
+    ----------
+    positions_p : np.ndarray
+        A 2D NumPy array of shape (n_particles, 3) containing the positions 
+        of particles in 3D space in units of kpc.
+    masses_p : np.ndarray
+        A 1D NumPy array of shape (n_particles,) containing the masses 
+        of the particles in units of solar masses.
+
+    Notes
+    -----
+    This function creates the root node of the octree, inserts all particles, 
+    and calculates the mass and center of mass for the entire tree.
+    """
+    cdef OctreeNode* root
+    cdef int n  # number of particles in tree
+
+    def __init__(self, positions_p: np.ndarray, masses_p: np.ndarray):
+        """
+        Initialize the octree and insert particles into it.
+        """
+        cdef int i
+        cdef int n = positions_p.shape[0]
+        cdef double[3] center
+        cdef double size
+        
+        self.n = n
+
+        # get centre of mass of all particles
+        center[0], center[1], center[2] = np.average(positions_p, axis=0, weights=masses_p)
+        
+        # bounding box size (twice the actial extent, since the COM isn't exactly 0
+        size = np.max(np.ptp(positions_p, axis=0)) * 2.
+
+        # convert input positions and masses to C arrays
+        cdef double[:, :] positions = positions_p
+        cdef double[:] masses = masses_p
+
+        del positions_p
+        del masses_p
+
+        # create the root node of the octree
+        self.root = create_octree_node(center, size, 1)
+
+        # insert each particle into the octree
+        for i in range(n):
+            insert(self.root, &positions[i, 0], masses[i])
+
+        # compute mass and center of mass over the entire tree
+        compute_mass_com(self.root)
+
+    def compute_potentials(self, positions_p: np.ndarray, njobs: int = 1):
+        """
+        Calculate the gravitational potential at specified positions.
+
+        Parameters
+        ----------
+        positions_p : np.ndarray
+            A 2D NumPy array of shape (n_points, 3) containing the positions 
+            where the gravitational potential should be calculated in units of kpc.
+        njobs : int, optional
+            Number of parallel jobs to use for the calculation (default is 1).
+            Currently, parallel computation (`njobs > 1`) is not implemented.
+
+        Returns
+        -------
+        np.ndarray
+            A 1D NumPy array of shape (n_points,) containing the gravitational 
+            potential at each input position.
+
+        Notes
+        -----
+        The gravitational potential is calculated using the Barnes-Hut approximation.
+        For now, only single-threaded execution is supported.
+        """
+        cdef int n = self.n  # Use same n from the initial octree build
+        cdef int n_parts = len(positions_p)
+        
+        # convert input positions to C array
+        cdef double[:, :] positions = positions_p
+        del positions_p
+        cdef double[:] potentials = np.zeros(n_parts, dtype=np.float64) # array to hold potentials
+        
+        if njobs > 1:
+            raise NotImplementedError("njobs > 1 not implemented yet.")
+            #compute_potential_parallel(self.root, positions, n_parts, potentials, njobs=njobs)
+        else:   
+            compute_potential_serial(self.root, positions, n_parts, potentials)
+
+        return np.array(potentials)
+    
+    def __dealloc__(self):
+        """
+        free the allocated memory for the octree.
+        """
+        free_octree(self.root)  # Free the octree structure
+        
+        
+cdef void compute_potential_serial(OctreeNode* root, double[:, :] positions, int n_parts, double[:] potentials):
+    '''
+    compute the gravitational potential for a batch of particles
+    '''
+    cdef double[3] r_vec
+    cdef double r_sq
+    cdef double r
+    cdef int i, j
+
+    # allocate a stack array with the maximum possible size of the number of particles used to build the tree
+    cdef int stack_capacity = 2 * MAX_OCTREE_DEPTH # heuristic for largest expected stack size
+    cdef OctreeNode** stack = <OctreeNode**> malloc(stack_capacity * sizeof(OctreeNode*))
+    if stack is NULL:
+        raise MemoryError("Failed to allocate initial stack")
+    cdef int stack_size = 0
+    cdef OctreeNode* current_node
+    
+    try:
+        for i in range(n_parts):
+            stack_size = 0  # reset stack size
+
+            # push the root node onto stack
+            stack[stack_size] = root
+            stack_size += 1
+
+            while stack_size > 0:
+                current_node = stack[stack_size - 1]  # pop the current node
+                stack_size -= 1
+
+                if current_node.mass == 0.0:
+                    continue
+
+                # distance between particle and node COM
+                for j in range(3):
+                    r_vec[j] = positions[i, j] - current_node.com[j]
+
+                r_sq = r_vec[0] * r_vec[0] + r_vec[1] * r_vec[1] + r_vec[2] * r_vec[2]
+
+                if r_sq == 0:  # skip self-interaction
+                    continue
+
+                r = sqrt(r_sq)
+
+                # apply the Barnes-Hut approximation (if needed)
+                if (current_node.size / r) < THETA:
+                    potentials[i] += -G * current_node.mass / r 
+                else:
+                    for j in range(8):
+                        if current_node.children[j] is not NULL:
+                            if stack_size >= stack_capacity:
+                                stack_capacity *= 2
+                                stack = <OctreeNode**> realloc(stack, stack_capacity * sizeof(OctreeNode*))
+                                if stack is NULL:
+                                    raise MemoryError("Failed to reallocate memory for stack")
+                            stack[stack_size] = current_node.children[j]  # push child node to stack
+                            stack_size += 1
+    finally:
+        free(stack)  # free memory allocated for stack
+    
 # C struct for Particle
 cdef struct Particle:
     double[3] position
@@ -44,6 +206,8 @@ cdef OctreeNode* create_octree_node(double[3] center, double size, int depth):
     '''
     cdef int i
     cdef OctreeNode* node = <OctreeNode*>malloc(sizeof(OctreeNode))
+    if node is NULL:
+        raise MemoryError("Failed to allocate memory for OctreeNode")
         
     node.center[0] = center[0]
     node.center[1] = center[1]
@@ -59,27 +223,11 @@ cdef OctreeNode* create_octree_node(double[3] center, double size, int depth):
     node.children = <OctreeNode**>malloc(8 * sizeof(OctreeNode*))  # allocate memory for 8 child nodes
     if node.children is NULL:
         free(node)
-    
+        raise MemoryError("Failed to allocate memory for child nodes")
     for i in range(8):
         node.children[i] = NULL  # each child initialized as NULL
     
     return node
-
-cdef void free_octree(OctreeNode* node):
-    '''
-    frees memory recursively
-    '''
-    cdef int i
-    
-    if node is NULL:
-        return
-    for i in range(8):
-        if node.children[i] is not NULL:
-            free_octree(node.children[i])
-    if node.particles is not NULL:
-        free(node.particles)
-    free(node.children)
-    free(node)
 
 cdef void insert(OctreeNode* node, double[3] position, double mass):
     '''
@@ -90,6 +238,8 @@ cdef void insert(OctreeNode* node, double[3] position, double mass):
         # insert particle into the current node, don't subdivide further
         if node.particles is NULL:
             node.particles = <Particle*>malloc(sizeof(Particle))
+            if node.particles is NULL:
+                raise MemoryError("Failed to allocate memory for particles in leaf node.")
             node.particles[0].position = position
             node.particles[0].mass = mass
             node.mass = mass
@@ -99,6 +249,8 @@ cdef void insert(OctreeNode* node, double[3] position, double mass):
             # add particle to the existing list
             node.num_particles += 1
             node.particles = <Particle*>realloc(node.particles, node.num_particles * sizeof(Particle))
+            if node.particles is NULL:
+                raise MemoryError("Failed to reallocate memory for particles in leaf node.")
             node.particles[node.num_particles - 1].position = position
             node.particles[node.num_particles - 1].mass = mass
             node.mass += mass
@@ -191,155 +343,19 @@ cdef void compute_mass_com(OctreeNode* node):
 
     node.mass = total_mass
 
-cdef void compute_potential_serial(OctreeNode* root, double[:, :] positions, int n_parts, double[:] potentials):
-    '''
-    compute the gravitational potential for a batch of particles
-    '''
-    cdef double[3] r_vec
-    cdef double r_sq
-    cdef double r
-    cdef int i, j
-
-    # allocate a stack array with the maximum possible size of the number of particles used to build the tree
-    cdef OctreeNode** stack = <OctreeNode**> malloc(MAX_OCTREE_DEPTH * 10 * sizeof(OctreeNode*))
-    cdef int stack_size = 0
-    cdef OctreeNode* current_node
-
-    for i in range(n_parts):
-        stack_size = 0  # reset stack size
-
-        # push the root node onto stack
-        stack[stack_size] = root
-        stack_size += 1
-
-        while stack_size > 0:
-            current_node = stack[stack_size - 1]  # pop the current node
-            stack_size -= 1
-
-            if current_node.mass == 0.0:
-                continue
-
-            # distance between particle and node COM
-            for j in range(3):
-                r_vec[j] = positions[i, j] - current_node.com[j]
-
-            r_sq = r_vec[0] * r_vec[0] + r_vec[1] * r_vec[1] + r_vec[2] * r_vec[2]
-            
-            if r_sq == 0:  # skip self-interaction
-                continue
-
-            r = sqrt(r_sq)
-            
-            # apply the Barnes-Hut approximation (if needed)
-            if (current_node.size / r) < THETA:
-                potentials[i] += -G * current_node.mass / r 
-            else:
-                for j in range(8):
-                    if current_node.children[j] is not NULL:
-                        stack[stack_size] = current_node.children[j]  # push child node to stack
-                        stack_size += 1
-
-    free(stack)  # free memory allocated for stack
     
-                
-cdef class Octree:
-    """
-    Initialize the octree and insert particles into it.
-
-    Parameters
-    ----------
-    positions_p : np.ndarray
-        A 2D NumPy array of shape (n_particles, 3) containing the positions 
-        of particles in 3D space in units of kpc.
-    masses_p : np.ndarray
-        A 1D NumPy array of shape (n_particles,) containing the masses 
-        of the particles in units of solar masses.
-
-    Notes
-    -----
-    This function creates the root node of the octree, inserts all particles, 
-    and calculates the mass and center of mass for the entire tree.
-    """
-    cdef OctreeNode* root
-    cdef int n  # number of particles in tree
-
-    def __init__(self, positions_p: np.ndarray, masses_p: np.ndarray):
-        """
-        Initialize the octree and insert particles into it.
-        """
-        cdef int i
-        cdef int n = positions_p.shape[0]
-        cdef double[3] center
-        cdef double size
-        
-        self.n = n
-
-        # get centre of mass of all particles
-        center[0], center[1], center[2] = np.average(positions_p, axis=0, weights=masses_p)
-        
-        # bounding box size (twice the actial extent, since the COM isn't exactly 0
-        size = np.max(np.ptp(positions_p, axis=0)) * 2.
-
-        # convert input positions and masses to C arrays
-        cdef double[:, :] positions = positions_p
-        cdef double[:] masses = masses_p
-
-        del positions_p
-        del masses_p
-
-        # create the root node of the octree
-        self.root = create_octree_node(center, size, 1)
-
-        # insert each particle into the octree
-        for i in range(n):
-            insert(self.root, &positions[i, 0], masses[i])
-
-        # compute mass and center of mass over the entire tree
-        compute_mass_com(self.root)
-
-    def compute_potentials(self, positions_p: np.ndarray, njobs: int = 1):
-        """
-        Calculate the gravitational potential at specified positions.
-
-        Parameters
-        ----------
-        positions_p : np.ndarray
-            A 2D NumPy array of shape (n_points, 3) containing the positions 
-            where the gravitational potential should be calculated in units of kpc.
-        njobs : int, optional
-            Number of parallel jobs to use for the calculation (default is 1).
-            Currently, parallel computation (`njobs > 1`) is not implemented.
-
-        Returns
-        -------
-        np.ndarray
-            A 1D NumPy array of shape (n_points,) containing the gravitational 
-            potential at each input position.
-
-        Notes
-        -----
-        The gravitational potential is calculated using the Barnes-Hut approximation.
-        For now, only single-threaded execution is supported.
-        """
-        cdef int n = self.n  # Use same n from the initial octree build
-        cdef int n_parts = len(positions_p)
-        
-        # convert input positions to C array
-        cdef double[:, :] positions = positions_p
-        del positions_p
-        cdef double[:] potentials = np.zeros(n_parts, dtype=np.float64) # array to hold potentials
-        
-        if njobs > 1:
-            raise NotImplementedError("njobs > 1 not implemented yet.")
-            return
-            #compute_potential_parallel(self.root, positions, n_parts, potentials, njobs=njobs)
-        else:   
-            compute_potential_serial(self.root, positions, n_parts, potentials)
-
-        return np.array(potentials)
+cdef void free_octree(OctreeNode* node):
+    '''
+    frees memory recursively
+    '''
+    cdef int i
     
-    def __dealloc__(self):
-        """
-        free the allocated memory for the octree.
-        """
-        free_octree(self.root)  # Free the octree structure
+    if node is NULL:
+        return
+    for i in range(8):
+        if node.children[i] is not NULL:
+            free_octree(node.children[i])
+    if node.particles is not NULL:
+        free(node.particles)
+    free(node.children)
+    free(node)
